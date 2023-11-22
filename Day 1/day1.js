ed40a9176babb5eee239928ffb5e4d6551d5e9ed
G-nizam-A <nizamcs10@gmail.com>
2023-11-22 20:14:18 +0530

    day 1

---
+++ Day 1/day1.js
@@ -0,0 +1,63 @@
+firstName = "Afreen"
+age = 1234567890
+isTrue = true
+phone = BigInt(1234567890)
+sym = Symbol('#')
+
+
+// console.log(firstName)
+// console.log(isTrue);
+// console.log(age)
+// console.log(phone)
+// console.log(sym);
+
+// console.log(typeof firstName)
+// console.log(typeof isTrue);
+// console.log(typeof age)
+// console.log(typeof phone)
+// console.log(typeof sym);
+
+// nizam = null
+// console.log(typeof nizam)
+
+
+// console.log(price); //hoisting
+// var price = 30;
+// console.log(price);
+// var price = 20;
+
+// console.log(price);
+
+// let name = "afreena";
+// console.log(name)
+// name = "some name";
+// console.log(name)
+
+let a;
+console.log(a)
+a = 20;
+console.log(a);
+
+const NAQHI = "w";
+console.log(NAQHI);
+
+// const PI = 3.14;
+// console.log(PI) 
+
+
+
+
+
+// let 
+
+// const
+
+
+
+
+
+
+
+
+
+
