97ae58f4687e3be63b41df775229604d6927ff51
G-nizam-A <nizamcs10@gmail.com>
2023-11-22 20:14:46 +0530

    Day 2

---
+++ Day 2/day2.js
@@ -0,0 +1,65 @@
+//let     //local scope
+//const   //local scope
+
+//var     //global scope
+
+// global variable
+// let cc = 10; // global variable
+// var name = "BBLEADS";
+// let price = 1000;
+// global scope
+
+// local variable
+// let a = 22;
+// {
+//   let a = 10;
+//   console.log("local", a);
+//   name = a;
+//   console.log("local", a);
+//   // a = 20;
+
+//   // scope / block scope / local scope
+// }
+// // global scope
+// console.log("global", a);
+// for(let i = 0; i<10;i++ ){// local variable
+
+//     // scope / block scope / loacl scope
+// }
+
+// // global scope
+
+// function aa(a){
+//     let b = 20;  // local variable
+//     // scope / block scope / loacl scope
+// }
+// // global scope
+
+// {
+//     let a;
+//     var b = 20;
+// }
+
+// console.log(b)
+
+// global scope
+
+// var vg = 88;         //global variable
+// let lg = 99;         //global variable
+// const cg = 22;       //global variable
+
+{
+    // var vg = 55;     //local variable
+    // let lg = 55;     //local variable
+    // const cg = 33;   //local variable
+
+    // console.log('var: local',vg)
+    // console.log('let: local',lg)
+    // console.log('const: local',cg)
+
+    // local scope
+}
+
+// console.log('var: global',vg)
+// console.log('let: global',lg)
+// console.log('const: global',cg)
